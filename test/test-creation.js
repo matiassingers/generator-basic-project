dc81c372cf20fb6bc1eb788443427558a8f69c29
Matias Singers <mail@matiassingers.com>
2014-11-05 18:30:15 +0800

    Create initial creation unit test

---
+++ test/test-creation.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('basic project generator', function () {
+  beforeEach(function (done) {
+    helpers.run(path.join( __dirname, '../app'))
+      .inDir(path.join( __dirname, './tmp'))
+      .on('end', done);
+  });
+
+  it('creates expected default files', function () {
+    var expected = [
+      '.gitignore',
+      '.editorconfig',
+      'readme.md',
+      'license',
+    ];
+
+    helpers.assertFiles(expected);
+  });
+});
